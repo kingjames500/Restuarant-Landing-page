920a146ac556e60ef8df685296c1b0b66fd359ee
kingjames500 <jimmymaina500@gmail.com>
2024-11-23 15:17:30 +0300

    adding component folder and images folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+
+import Navbar from "./component/Header/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <h2>the full component should be here.</h2>
-    </div>
+    <Navbar />
   );
 }
 
