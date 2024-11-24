490d9dbd81d40ea08071d8e998d71e908d8ce990
Salutah <salutahmoha@gmail.com>
2024-11-24 07:16:08 +0300

    Add copyrights-section

--- src/component/Copyrights/Copyrights.jsx
+++ src/component/Copyrights/Copyrights.jsx
@@ -1,22 +1,22 @@
-// import React from 'react'
-// import './Copyrights.css'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faFacebookF, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import React from 'react'
+import './Copyrights.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
-// function Copyrights() {
-//   return (
-//     <div className='copyright-container'>
-//         <div className='copyright-socials'>
-//             <div className='social-media-handles facebook'><FontAwesomeIcon icon={faFacebookF} /></div>
-//             <div className='social-media-handles'><FontAwesomeIcon icon={faInstagram} /></div>
-//             <div className='social-media-handles'><FontAwesomeIcon icon={faTwitter} /></div>
-//             <div className='social-media-handles'><FontAwesomeIcon icon={faYoutube} /></div>
-//         </div>
-//         <div className='copyright-text'>
-//             <p>Copyright       2023 Dscode | All rights reserved</p>
-//         </div>
-//     </div>
-//   )
-// }
+function Copyrights() {
+  return (
+    <div className='copyright-container'>
+        <div className='copyright-socials'>
+            <div className='social-media-handles facebook'><FontAwesomeIcon icon={faFacebookF} /></div>
+            <div className='social-media-handles'><FontAwesomeIcon icon={faInstagram} /></div>
+            <div className='social-media-handles'><FontAwesomeIcon icon={faTwitter} /></div>
+            <div className='social-media-handles'><FontAwesomeIcon icon={faYoutube} /></div>
+        </div>
+        <div className='copyright-text'>
+            <p>Copyright       2023 Dscode | All rights reserved</p>
+        </div>
+    </div>
+  )
+}
 
-// export default Copyrights
+export default Copyrights
