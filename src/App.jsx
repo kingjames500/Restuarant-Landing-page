6c4373b916fdec9eb4bd029f44c1fdbc4c55126c
Salutah <salutahmoha@gmail.com>
2024-11-24 09:57:20 +0300

    Adding changes to styling in services and copyrights

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,16 @@ import Header from "./component/Header/Header";
 import HeroSection from "./component/Hero/Hero";
 import CustomerFavourites from "./component/CustomerFavourites/CustomerFavourites";
 import SpecialDishes from "./component/Menu/Menu";
-<<<<<<< HEAD
 
 import Services from "./component/Services/Services";
 import Footer from "./component/Footer/Footer";
 import Copyrights from "./component/Copyrights/Copyrights";
-
 import Testimonials from "./component/Testimonials/Testimonials";
 
-=======
 import Testimonials from "./component/Testimonials/Testimonials";
 import Services from "./component/Services/Services";
 import Footer from "./component/Footer/Footer";
 import Copyrights from "./component/Copyrights/Copyrights";
->>>>>>> 330032e2f97d49229f17cb6cf0c0323fe02a14e5
 function App() {
   return (
     <>
@@ -27,12 +23,8 @@ function App() {
       <Services />
       <Footer />
       <Copyrights />
-<<<<<<< HEAD
-=======
-     
-
->>>>>>> 330032e2f97d49229f17cb6cf0c0323fe02a14e5
     </>
+    
   );
 }
 
