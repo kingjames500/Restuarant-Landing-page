00e9e954bfbd90745f2096e56e6bc09aed9e1a1e
kingjames500 <jimmymaina500@gmail.com>
2024-11-24 15:45:27 +0300

    resolving conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import HeroSection from "./component/Hero/Hero";
 import CustomerFavourites from "./component/CustomerFavourites/CustomerFavourites";
 import SpecialDishes from "./component/Menu/Menu";
 
-import Services from "./component/Services/Services";
-import Footer from "./component/Footer/Footer";
-import Copyrights from "./component/Copyrights/Copyrights";
-import Testimonials from "./component/Testimonials/Testimonials";
+// import Services from "./component/Services/Services";
+// import Footer from "./component/Footer/Footer";
+// import Copyrights from "./component/Copyrights/Copyrights";
+// import Testimonials from "./component/Testimonials/Testimonials";
 
 import Testimonials from "./component/Testimonials/Testimonials";
 import Services from "./component/Services/Services";
@@ -24,7 +24,6 @@ function App() {
       <Footer />
       <Copyrights />
     </>
-    
   );
 }
 
